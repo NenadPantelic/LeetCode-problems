5be7620e4b294da68b1ce43c7b7e9366b58806f9
NenadPantelic <nenadpantelickg@gmail.com>
2020-05-30 12:46:16 +0200

    Add new exercise - 973 (medium)

---
+++ 973.KClosestPointstoOrigin.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 12:30:54 2020
+
+@author: nenad
+"""
+
+
+"""
+
+Problem URL: https://leetcode.com/problems/k-closest-points-to-origin/
+Problem description:  
+K Closest Points to Origin
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+ 
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation: 
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+ 
+
+Note:
+
+1 <= K <= points.length <= 10000
+-10000 < points[i][0] < 10000
+-10000 < points[i][1] < 10000
+
+"""
+from math import sqrt
+class Solution:
+    def euclideanDistance(self, point):
+        return sqrt(point[0] ** 2 + point[1] ** 2)
+    def kClosest(self, points, K: int):
+        pointsWithDist = []
+        for point in points:
+            pointsWithDist.append((self.euclideanDistance(point), point))
+                    
+        # sort points by distance and obtain first K points
+        sortedPoints = sorted(pointsWithDist, key=lambda x:x[0])[:K]
+        # get only points (without distances)
+        return [point[1] for point in sortedPoints]
+        
+        return sortedPoints
+    
+sol = Solution()
+
+# Test 1
+points = [[1,3],[-2,2]]; K = 1
+print(sol.kClosest(points, K))
+            
+
+# Test 2
+points = [[3,3],[5,-1],[-2,4]]; K = 2
+print(sol.kClosest(points, K))
+             
+# Test 3
+points = [[1,3],[-2,2],[2,-2]]; K = 2
+print(sol.kClosest(points, K))
